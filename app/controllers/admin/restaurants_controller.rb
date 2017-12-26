2384d9f5ceaef72e857ea3e1b1bdcd4bd48618d7
eating <sstar0403@gmail.com>
2017-12-27 01:45:42 +0800

    set up admin authentication

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,4 +1,7 @@
 class Admin::RestaurantsController < ApplicationController
+  before_action :authenticate_user! #Devise提供的方法，檢查使用者是否登入
+  before_action :authenticate_admin #手工定義的方法，檢查使用者是否為管理者
+
   def index
   end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,13 @@ class RestaurantsController < ApplicationController
   before_action :authenticate_user!
   def index
   end
+
+  private
+
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = "Not allow!"
+      redirct_to root_path
+    end
+  end
 end
