99c2a75de6f293419fb07189483a90a348f3734f
eating <sstar0403@gmail.com>
2018-01-12 00:15:36 +0800

    add category index page for admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,8 @@
 class Admin::CategoriesController < ApplicationController
   before_action :authenticate_user! #Devise提供的方法，檢查使用者是否登入
   before_action :authenticate_admin #手工定義的方法，檢查使用者是否為管理者
+
+  def index
+    @categories = Category.all
+  end
 end
