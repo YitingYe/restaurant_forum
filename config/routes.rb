3a98c491d82d12027e9b32658e9b7d2847f60268
eating <sstar0403@gmail.com>
2018-01-21 13:28:21 +0800

    user can update profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: [:show, :edit, :upload]
+  resources :users, only: [:show, :edit, :update]
 
   resources :restaurants, only: [:index, :show]  
   root "restaurants#index"
